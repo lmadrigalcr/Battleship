fee8e5cb179b4e455017c49db1b36994c20ca7c2
Emmurillo <emmanuel-ms@hotmail.com>
2015-10-22 21:25:09 -0600

    Retrieve rows and cols from file

--- filereader.pl
+++ filereader.pl
@@ -1,8 +1,8 @@
-main(FileName) :-
+main(Lines, FileName) :-
     open(FileName, read, Str),
     read_file(Str,Lines),
     close(Str),
-    write(Lines), !.
+    !.
 
 read_file(end_of_file,_ ):- !.
 
@@ -13,3 +13,15 @@ read_file(Stream,[X|L]) :-
     \+at_end_of_stream(Stream),
     read(Stream,X),
     read_file(Stream,L).
+
+get_cols([_,H|_], H).
+get_cols(C) :-
+  main(Xs, 'opponent'),
+  get_cols(Xs, Ys),
+  atom_chars(Ys,C).
+
+get_rows([H|_], H).
+get_rows(R) :-
+  main(Xs, 'opponent'),
+  get_rows(Xs, Ys),
+  atom_chars(Ys,R).
