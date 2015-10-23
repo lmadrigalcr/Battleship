76ea57a0a735db294874ecff626e9497480edb10
Emmurillo <emmanuel-ms@hotmail.com>
2015-10-23 08:17:23 -0600

    Cast chars to numbers

--- filereader.pl
+++ filereader.pl
@@ -15,16 +15,23 @@ read_file(Stream,[X|L]) :-
     read_file(Stream,L).
 
 get_board(B):-
-  main(B,'my_board'). 
+  main(B,'my_board').
 
 get_cols([_,H|_], H).
 get_cols(C) :-
   main(Xs, 'opponent'),
   get_cols(Xs, Ys),
-  atom_chars(Ys,C).
+  atom_chars(Ys,Zs),
+  convert(Zs, C).
 
 get_rows([H|_], H).
 get_rows(R) :-
   main(Xs, 'opponent'),
   get_rows(Xs, Ys),
-  atom_chars(Ys,R).
+  atom_chars(Ys,Zs),
+  convert(Zs, R).
+
+convert([],[]).
+convert([H1|T1], [H2|T2]) :-
+  convert(T1, T2),
+  atom_number(H1,H2).
